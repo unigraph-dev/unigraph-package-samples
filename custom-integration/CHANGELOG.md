47df02a0823ddca3279176a67aec732feb80ac3d
Sophia Xu <i@sophiaxu.me>
2022-04-03 10:52:21 -0400

    Add sample Reddit integration

---
+++ custom-integration/CHANGELOG.md
@@ -0,0 +1,64 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+## [0.2.5](https://github.com/unigraph-dev/unigraph-dev/compare/v0.2.4...v0.2.5) (2022-03-13)
+
+**Note:** Version bump only for package unigraph.reddit
+
+
+
+
+
+## [0.2.4](https://github.com/unigraph-dev/unigraph-dev/compare/v0.2.3...v0.2.4) (2022-03-13)
+
+**Note:** Version bump only for package unigraph.reddit
+
+
+
+
+
+## [0.2.3](https://github.com/unigraph-dev/unigraph-dev/compare/v0.2.2...v0.2.3) (2022-03-11)
+
+**Note:** Version bump only for package unigraph.reddit
+
+
+
+
+
+## [0.2.2](https://github.com/unigraph-dev/unigraph-dev/compare/v0.2.1...v0.2.2) (2022-03-09)
+
+**Note:** Version bump only for package unigraph.reddit
+
+
+
+
+
+## [0.2.1](https://github.com/unigraph-dev/unigraph-dev/compare/v0.2.0...v0.2.1) (2022-03-09)
+
+**Note:** Version bump only for package unigraph.reddit
+
+
+
+
+
+# [0.2.0](https://github.com/unigraph-dev/unigraph-dev/compare/v0.1.0...v0.2.0) (2022-03-07)
+
+**Note:** Version bump only for package unigraph.reddit
+
+
+
+
+
+# [0.1.0](https://github.com/unigraph-dev/unigraph-dev/compare/v0.1.10...v0.1.0) (2022-02-21)
+
+
+### Features
+
+* **feeds:** created a Feeds list entity and redirected feeds from inbox to it ([68a5de4](https://github.com/unigraph-dev/unigraph-dev/commit/68a5de4bb576f6a0159ace256f3527f63fd73410))
+
+
+### Reverts
+
+* Revert "docs: changelogs initialized properly" ([e5b8921](https://github.com/unigraph-dev/unigraph-dev/commit/e5b89215d19fb7478cd76898e6473544f21c773e))
---
+++ custom-integration/executables/updateRedditSubscriptions.js
@@ -0,0 +1,138 @@
+const account = (
+    await unigraph.getQueries([
+        `(func: uid(accs)) @cascade {
+    uid
+    type {<unigraph.id>}
+    _value {
+        site {
+            _value {
+                _value {
+                    name @filter(eq(<_value.%>, "Reddit")) {
+                        <_value.%>
+                    }
+                }
+            }
+        }
+        access_token { <_value.%> }
+        token_expires_in { <_value.%dt> }
+        refresh_token { <_value.%> }
+        subscriptions {
+            <_value[> {
+                _value {
+                    uid
+                    _value {
+                        name { <_value.%> }
+                        last_id_fetched { <_value.%> }
+                    }
+                }
+            }
+        }
+    }
+}
+
+var(func: eq(<unigraph.id>, "$/schema/internet_account")) {
+    <~type> { accs as uid }
+}`,
+    ])
+)?.[0]?.[0];
+
+if (account?.uid) {
+    // Check if access token is expired
+    let token = account._value.access_token['_value.%'];
+    const lastFetched = account._value.subscriptions['_value['][0]._value._value.last_id_fetched['_value.%'];
+    const beforeObj = ''; // lastFetched.length ? "&before="+lastFetched : ""
+    const fetch = require('node-fetch');
+    if (true) {
+        const appClientId = unigraph.getSecret('reddit', 'client_id');
+        const resp = await fetch('https://www.reddit.com/api/v1/access_token', {
+            method: 'POST',
+            body: `grant_type=refresh_token&refresh_token=${account._value.refresh_token['_value.%']}`,
+            headers: {
+                Authorization: `Basic ${Buffer.from(`${appClientId}:`).toString('base64')}`,
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            },
+        });
+        const accessTokenResult = await resp.json();
+        token = accessTokenResult.access_token;
+        await unigraph.updateObject(account.uid, {
+            access_token: token,
+            token_expires_in: new Date(new Date().getTime() + 3600 * 1000).toISOString(),
+        });
+    }
+    // TODO: update reddit subscriptions
+    const subs = await fetch(`https://oauth.reddit.com/?limit=60${beforeObj}`, {
+        headers: {
+            Authorization: `bearer ${token}`,
+        },
+    });
+    const subJson = await subs.json();
+    const updatedObjs = subJson.data.children
+        .map((el) => el.data)
+        .map((el) => {
+            return {
+                _updatedAt: new Date(el.created * 1000).toISOString(),
+                name: {
+                    type: { 'unigraph.id': '$/schema/markdown' },
+                    _value: el.title,
+                },
+                selftext: {
+                    type: { 'unigraph.id': '$/schema/markdown' },
+                    _value: el.selftext,
+                },
+                ...(el.thumbnail ? { thumbnail: el.thumbnail } : {}),
+                subreddit: {
+                    name: el.subreddit,
+                },
+                permalink: `https://reddit.com${el.permalink}`,
+                url: el.url,
+                id: el.name,
+            };
+        });
+    // Add these items to Unigraph
+    const getQuery = (id) => `(func: uid(redd)) @cascade { 
+        uid
+        _value {
+          id @filter(eq(<_value.%>, "${id}"))
+        }
+    }`;
+
+    const results = await unigraph.getQueries(
+        [
+            ...updatedObjs.map((el) => getQuery(el.id)),
+            `var(func: eq(<unigraph.id>, "$/schema/reddit_post")) {
+        <~type> (first: -250) { redd as uid }
+    }`,
+        ],
+        false,
+        100,
+    );
+
+    const count = updatedObjs.length;
+    const uids = await unigraph.addObject(
+        updatedObjs.filter((el, index) => results[index]?.length === 0),
+        '$/schema/reddit_post',
+    );
+    const feeds_els = uids;
+
+    if (updatedObjs?.[0]?.id) {
+        await unigraph.updateObject(
+            account._value.subscriptions['_value['][0]._value.uid,
+            {
+                _value: {
+                    last_id_fetched: {
+                        '_value.%': updatedObjs[0].id,
+                    },
+                },
+            },
+            true,
+            false,
+        );
+    }
+
+    console.log(`Updated items: ${feeds_els.length}`);
+
+    await unigraph.runExecutable('$/executable/add-item-to-list', {
+        where: '$/entity/feeds',
+        item: feeds_els.reverse(),
+    });
+}
